691289275a23d99734edd37dce6efb30a6ab2091
IamPeterHwang <ken.lmstar11@gmail.com>
2015-08-21 14:09:16 +0900

    LUT processing, Create control and Draw line/rectangle

--- MIPL_MFC/ChildView.cpp
+++ MIPL_MFC/ChildView.cpp
@@ -16,14 +16,14 @@
 
 CChildView::CChildView()
 {
-	dibData		= NULL; // constructor, initialize pointer to NULL
+	dibData		= NULL;		// constructor, initialize pointer to NULL
 	dstData		= NULL;
 }
 
 CChildView::~CChildView()
 {
 	if(dibData)
-		delete[] dibData; // destructor, remove allocated memory if program ends
+		delete[] dibData;	// destructor, remove allocated memory if program ends
 	if(dstData)
 		delete[] dstData;
 }
@@ -31,7 +31,8 @@ CChildView::~CChildView()
 
 BEGIN_MESSAGE_MAP(CChildView, CWnd)
 		ON_WM_PAINT()
-	
+	ON_WM_CREATE()
+	ON_WM_HSCROLL()
 	ON_COMMAND(ID_TEST_TEST,					OnTestTest)
 	ON_COMMAND(ID_FILE_OPEN,					OnFileOpen)
 	ON_COMMAND(ID_ARITHMETIC_ADD,				OnArithmeticAdd)
@@ -44,9 +45,19 @@ BEGIN_MESSAGE_MAP(CChildView, CWnd)
 	ON_COMMAND(ID_GEOMETRIC_FLIPH,				OnGeometricFlipH)
 	ON_COMMAND(ID_GEOMETRIC_ROTATELEFT,			OnGeometricRotateLeft)
 	ON_COMMAND(ID_GEOMETRIC_ROTATERIGHT,		OnGeometricRotateRight)
-
+	ON_COMMAND(ID_LUT_ADD, &CChildView::OnLutAdd)
+	ON_COMMAND(ID_LUT_SUB, &CChildView::OnLutSub)
+	ON_COMMAND(ID_LUT_MULTIPLY, &CChildView::OnLutMultiply)
+	ON_COMMAND(ID_LUT_DIVIDE, &CChildView::OnLutDivide)
+	ON_COMMAND(ID_LUT_NEGATIVE, &CChildView::OnLutNegative)
+	ON_COMMAND(ID_LUT_GAMMA, &CChildView::OnLutGamma)
+	ON_UPDATE_COMMAND_UI(ID_LUT_ADD, &CChildView::OnUpdateLutAdd)
+	ON_UPDATE_COMMAND_UI(ID_LUT_SUB, &CChildView::OnUpdateLutSub)
+	ON_UPDATE_COMMAND_UI(ID_LUT_MULTIPLY, &CChildView::OnUpdateLutMultiply)
+	ON_UPDATE_COMMAND_UI(ID_LUT_DIVIDE, &CChildView::OnUpdateLutDivide)
+	ON_UPDATE_COMMAND_UI(ID_LUT_NEGATIVE, &CChildView::OnUpdateLutNegative)
+	ON_UPDATE_COMMAND_UI(ID_LUT_GAMMA, &CChildView::OnUpdateLutGamma)
 END_MESSAGE_MAP()
-
 
 
 // CChildView message handlers
@@ -64,17 +75,72 @@ BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs)
 	return TRUE;
 }
 
+int CChildView::OnCreate(LPCREATESTRUCT lpCreateStruct)
+{
+	if (CWnd::OnCreate(lpCreateStruct) == -1)
+		return -1;
+
+	scrollBar.Create(SBS_HORZ | WS_VISIBLE | WS_CHILD, CRect(0, 520, 520, 540),
+		this, 9999);						// SBS_HORZ: type of scrollbar, CRect(left, top, right, bottom), 9999: scrollbar id.
+	scrollBar.SetScrollRange(-100, 200);	// Scrollbar total range, far left = -100, far right = 200.
+	scrollBar.SetScrollPos(0);				// Scrollbox(thumb) starting position.
+
+	return 0;
+}
+
 void CChildView::OnPaint() 
 {
 	CPaintDC dc(this); // device context for painting
+
 	
 	if(dibData == NULL)
-		return; // if dibData == NULL, end
+		return;									// if dibData == NULL, end
 
+	//switch (scrollBar.GetScrollPos())			// As soon as file is opend, synchronize scrollbox, then operate gammacorrection.
+	//{
+	//	case 0:
+	//		break;
+
+	//	default:
+	//		double gamma = 1 + scrollBar.GetScrollPos() / (double)100;
+	//		GammaCorrection( gamma );
+	//		break;
+	//}
+
+	//Invalidate(FALSE); 
+		
+	
 	::SetDIBitsToDevice(dc.m_hDC,
-		0, 0, width, height,
-		0, 0, 0, height,
-		dstData, bitmapInfo, DIB_RGB_COLORS); // print bmp file on screen 
+		0, 0, width, height,					// Destination
+		0, 0, 0, height,						// Source
+		dstData, bitmapInfo, DIB_RGB_COLORS);	// print bmp file on screen, that is, draw image
+	
+	
+	CPen	Pen;
+	CPen*	poldPen;
+	Pen.CreatePen(PS_SOLID, 8, RGB(0, 200, 255));
+	poldPen = dc.SelectObject(&Pen);
+
+	CBrush	Brush;
+	CBrush*	poldBrush;
+	Brush.CreateSolidBrush(RGB(255, 0, 0));
+	poldBrush = dc.SelectObject(&Brush);
+
+	{
+		dc.MoveTo(200, 200);
+		dc.LineTo(300, 300);
+
+		dc.Rectangle(10, 10, 50, 50);
+		dc.Ellipse(50, 50, 150, 150);
+		dc.RoundRect(100, 100, 220, 320, 30, 80);
+		dc.Pie(0, 0, 250, 250, 100, 50, 200, 100);
+	}
+	
+	dc.SelectObject(poldBrush);
+	Brush.DeleteObject();
+
+	dc.SelectObject(poldPen);
+	Pen.DeleteObject();
 
 	/*
 	// filling color on whole screen
@@ -95,7 +161,7 @@ void CChildView::OnPaint()
 void CChildView::OnTestTest()
 {
 	// show message
-	AfxMessageBox(L"Hello World");
+	AfxMessageBox(L"Test Word is Hello World");
 }
 
 void CChildView::OnFileOpen()
@@ -121,6 +187,7 @@ void CChildView::OnFileOpen()
 	// determine DIB size
 	int dibSize = bitmapFileHeader.bfSize - sizeof(BITMAPFILEHEADER);
 
+	// declare dib data pointer variable
 	// is dibData really zero? is memory empty?
 	if (dibData)
 		delete[] dibData;
@@ -137,7 +204,7 @@ void CChildView::OnFileOpen()
 	// dibData indicate first starting point of DIB structure, finally srcData indicate starting point of actual data
 	srcData			= dibData + sizeof(BITMAPINFOHEADER) + sizeof(RGBQUAD) * bitmapInfo->bmiHeader.biClrUsed;
 
-	// copy data ¼±¾ð.
+	// declare dst data pointer variable 
 	if(dstData)
 		delete[] dstData;
 	dstData = new unsigned char[step*height];
@@ -156,7 +223,8 @@ int CChildView::GetRealWidth(int width)  // get step
 	int real_width;
 	div_t r;
 	r = div(width,4);
-	if(r.rem != 0) {
+	if(r.rem != 0) 
+	{
 		real_width = ((r.quot + 1) * 4); // make multiple of 4, ex) in case to need 13 bytes, dump remainder, then quotient plus one and multiply 4, it is 4 multiple of 4 more than 13
 		return real_width;
 	}
@@ -179,7 +247,8 @@ void CChildView::OnArithmeticAdd() // pixel value + 50
 	if( dibData == NULL )
 		return;
 
-	for( int i=0; i < width * height; i++) {
+	for( int i=0; i < width * height; i++) 
+	{
 		dstData[i] = Clip(srcData[i] + 50, 0, 255);
 	}
 
@@ -191,7 +260,8 @@ void CChildView::OnArithmeticSub() // pixel value - 50
 	if( dibData == NULL )
 		return;
 
-	for( int i=0; i < width * height; i++) {
+	for( int i=0; i < width * height; i++) 
+	{
 		dstData[i] = Clip(srcData[i] - 50, 0, 255);
 	}
 
@@ -204,7 +274,8 @@ void CChildView::OnArithmeticMultiply() // pixel value * 5
 	if( dibData == NULL )
 		return;
 
-	for( int i=0; i < width * height; i++){
+	for( int i=0; i < width * height; i++)
+	{
 		dstData[i] = Clip(srcData[i] * 5, 0, 255);
 	}
 
@@ -217,7 +288,8 @@ void CChildView::OnArithmeticDivide() // pixel value / 4
 	if( dibData == NULL )
 		return;
 
-	for( int i=0; i < width * height; i++){
+	for( int i=0; i < width * height; i++)
+	{
 		dstData[i] = Clip(srcData[i] / 4, 0, 255);
 	}
 
@@ -230,7 +302,8 @@ void CChildView::OnArithmeticNegative() // reversal, 0 -> 255, 255 -> 0
 	if( dibData == NULL )
 		return;
 
-	for( int i=0; i < width * height; i++){
+	for( int i=0; i < width * height; i++)
+	{
 		dstData[i] = 255 - srcData[i];
 	}
 
@@ -238,23 +311,14 @@ void CChildView::OnArithmeticNegative() // reversal, 0 -> 255, 255 -> 0
 
 }
 
-unsigned char CChildView::Clip_r(double value, double low, double high) // parameter is real
-{
-	if(value < low)
-		return (unsigned char) low;
-	else if(value > high)
-		return (unsigned char) high;
-	else
-		return (unsigned char) value;
-}
-
 void CChildView::OnArithmeticGamma() // Gamma correction, because of hardware problem and etc, it appears convex, then must make 1st linear function  
 {
 	if( dibData == NULL )
 		return;
 
-	for( int i=0; i < width * height; i++) {
-		dstData[i] = Clip_r(255*pow(srcData[i]/255.0,1.0/2.2), 0.0, 255.0);
+	for( int i=0; i < width * height; i++) 
+	{
+		dstData[i] = Clip((int)(255 * pow(srcData[i] / (double)255, 1 / 2.2)), 0, 255);
 	}
 
 	Invalidate(FALSE);
@@ -294,6 +358,7 @@ void CChildView::OnGeometricRotateLeft() // Rotate Left
 		for(int j = 0 ; j < width ; j++)
 		{
 			dstData[(j*step) + (height - 1 - i)] = srcData[(i*step) + j];
+			// *(dstData(j*step + (height - 1 - i)) = *(srcData(i*step + j);
 		}
 	}
 
@@ -308,8 +373,187 @@ void CChildView::OnGeometricRotateRight() // Rotate Right
 		for(int j = 0 ; j < width ; j++)
 		{
 			dstData[(i*step) + j] = srcData[j*step + height - 1 - i];
+			// *(dstData(i*step + j)) = *(srcData(j*step + (height - 1 - i)));
 		}
 	}
 
 	Invalidate(FALSE);
+}
+
+void CChildView::OnLutAdd()				// create Add lut		
+{
+	unsigned char lut[256];				// declare lut array, size of array is 256, because of 8 bit
+	
+	for(int i = 0; i < 256; i++)
+		lut[i] = Clip(i + 50, 0, 255);
+
+	for(int i = 0; i < width * height; i++)
+		dstData[i] = lut[ srcData[i] ];
+
+	Invalidate(FALSE);
+}
+
+
+void CChildView::OnUpdateLutAdd(CCmdUI *pCmdUI)
+{
+	if( dibData == NULL)
+		pCmdUI->Enable (FALSE);			// if file or image is not opened, "Add" button in "LUT" tab is enable 
+}
+
+void CChildView::OnLutSub()				// create Sub lut
+{
+	unsigned char lut[256];
+
+	for(int i = 0; i < 256; i++)
+		lut[i] = Clip(i - 50, 0, 255);	
+
+	for(int i = 0; i < width * height; i++)
+		dstData[i] = lut[ srcData[i] ];
+
+	Invalidate(FALSE);
+}
+
+
+void CChildView::OnUpdateLutSub(CCmdUI *pCmdUI)
+{
+	if( dibData == NULL)
+		pCmdUI->Enable (FALSE);
+}
+
+
+void CChildView::OnLutMultiply()		// create Multiply lut
+{
+	unsigned char lut[256];
+
+	for(int i = 0; i < 256; i++)
+		lut[i] = Clip(i * 5, 0, 255);	
+
+	for(int i = 0; i < width * height; i++)
+		dstData[i] = lut[ srcData[i] ];
+
+	Invalidate(FALSE);
+}
+
+
+void CChildView::OnUpdateLutMultiply(CCmdUI *pCmdUI)
+{
+	if( dibData == NULL)
+		pCmdUI->Enable (FALSE);
+}
+
+
+void CChildView::OnLutDivide()			// create Divide lut
+{
+	unsigned char lut[256];
+
+	for(int i = 0; i < 256; i++)
+		lut[i] = Clip(i / 5, 0, 255);	
+
+	for(int i = 0; i < width * height; i++)
+		dstData[i] = lut[ srcData[i] ];
+
+	Invalidate(FALSE);
+}
+
+
+void CChildView::OnUpdateLutDivide(CCmdUI *pCmdUI)
+{
+	if( dibData == NULL)
+		pCmdUI->Enable (FALSE);
+}
+
+
+void CChildView::OnLutNegative()		// create Negative lut
+{
+	unsigned char lut[256];
+
+	for(int i = 0; i < 256; i++)
+		lut[i] = 255 - i;	
+
+	for(int i = 0; i < width * height; i++)
+		dstData[i] = lut[ srcData[i] ];
+
+	Invalidate(FALSE);
+}
+
+
+void CChildView::OnUpdateLutNegative(CCmdUI *pCmdUI)
+{
+	if( dibData == NULL)
+		pCmdUI->Enable (FALSE);
+}
+
+
+void CChildView::OnLutGamma()			// create Gamma lut
+{
+	double gamma = 2.2;
+
+	GammaCorrection( gamma );
+}
+
+void CChildView::GammaCorrection( double gamma )
+{
+	double			exp = 1 / gamma;
+	unsigned char	lut[256];
+	
+	for(int i = 0; i < 256; i++)
+		lut[i] = Clip((int)(255 * pow(i / (double)255, exp)), 0, 255);
+
+	for(int i = 0; i < width * height; i++)
+		dstData[i] = lut[ srcData[i] ];
+
+	Invalidate(FALSE);
+}
+
+void CChildView::OnUpdateLutGamma(CCmdUI *pCmdUI)
+{
+	if( dibData == NULL)
+		pCmdUI->Enable (FALSE);
+}
+
+void CChildView::OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar)
+{
+	// SCROLLINFO contains scroll bar parameter
+	SCROLLINFO info;
+	scrollBar.GetScrollInfo(&info, SIF_ALL);
+	
+	int minpos = info.nMin;
+	int maxpos = info.nMax;
+	int curpos = scrollBar.GetScrollPos();
+
+   // Determine the new position of scroll box.
+	switch (nSBCode)
+	{
+		case SB_LINELEFT:      // Scroll left
+			if (curpos > minpos)
+				curpos--;
+			break;
+		
+		case SB_LINERIGHT:      // Scroll right
+			if (curpos < maxpos)
+				curpos++;
+			break;
+
+		case SB_PAGELEFT:      // Scroll page(Shaft, in addition to Thumb) left
+			if (curpos > minpos)
+				curpos = max(minpos, curpos - 10);
+			break;
+
+		case SB_PAGERIGHT:      // Scroll page(Shaft, in addition to Thumb) right
+			if (curpos < maxpos)
+				curpos = min(maxpos, curpos + 10);
+			break;
+		
+		case SB_THUMBTRACK:   // Drag scroll box to specified position.
+			curpos = nPos;    // nPos is the position that the scroll box has been dragged to.
+			break;
+	}
+
+	scrollBar.SetScrollPos(curpos);	 // Set the new position of the thumb (scroll box).
+
+	double gamma = 1 + curpos / (double)100;
+	GammaCorrection( gamma );
+
+
+	CWnd::OnHScroll(nSBCode, nPos, pScrollBar);
 }
--- MIPL_MFC/ChildView.h
+++ MIPL_MFC/ChildView.h
@@ -10,31 +10,31 @@
 
 class CChildView : public CWnd
 {
-// Construction
+// Construction, constructor or initailizing function
 public:
 	CChildView();
 
-// Attributes
-public:
-	// public -> anywhere access
-	// struct BITMAPINFO : BITMAPINFOHEADER bmiHeader and RGBQUAD bmiColors[1]
-	BITMAPINFO*			bitmapInfo;
+// Attributes, member variable or Get/Set function
+public:									// public -> anywhere access
+	BITMAPINFO*			bitmapInfo;		// struct BITMAPINFO : BITMAPINFOHEADER bmiHeader and RGBQUAD bmiColors[1]
 
-	// dibData : BITMAPINFOHEADER + RGBQUAD + DATA
-	unsigned char*		dibData;
-	unsigned char*		srcData; // source data
-	unsigned char*		dstData; // copy(destination) data
+	unsigned char*		dibData;		// dibData : BITMAPINFOHEADER + RGBQUAD + DATA
+	unsigned char*		srcData;		// source data
+	unsigned char*		dstData;		// copy(destination) data
 
 	int					samplePerPixel; // number of byte of using to express 1 pixel
-	int					width; // pixel
-	int					height; // pixel
-	int					step; // size of one row, byte
+	int					width;			// pixel
+	int					height;			// pixel
+	int					step;			// size of one row, byte
 
-	int					GetRealWidth(int width); // get step
-	unsigned char		Clip(int value, int low, int high); // below low -> low, above high -> high 
-	unsigned char		Clip_r(double value, double low, double high);
+	int					GetRealWidth(int width);						// get step
+	unsigned char		Clip(int value, int low, int high);				// below low -> low, above high -> high 
 
-// Operations
+	void				GammaCorrection(double gamma);
+
+	CScrollBar			scrollBar;
+
+// Operations, member function occuring activity
 public:
 
 // Overrides
@@ -45,23 +45,41 @@ public:
 public:
 	virtual ~CChildView();
 
-	// Generated message map functions
+// Generated message map functions
 protected:
-	afx_msg void OnPaint();
-	DECLARE_MESSAGE_MAP()
+		afx_msg void OnPaint();
+	afx_msg int	OnCreate(LPCREATESTRUCT lpCreateStruct);
+	afx_msg void OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
+		DECLARE_MESSAGE_MAP()
 
 public:
 	afx_msg void OnTestTest();
+
 	afx_msg void OnFileOpen();
+
 	afx_msg void OnArithmeticAdd();
 	afx_msg void OnArithmeticSub();
 	afx_msg void OnArithmeticMultiply();
 	afx_msg void OnArithmeticDivide();
 	afx_msg void OnArithmeticNegative();
 	afx_msg void OnArithmeticGamma();
+
 	afx_msg void OnGeometricFlipV();
 	afx_msg void OnGeometricFlipH();
 	afx_msg void OnGeometricRotateLeft();
 	afx_msg void OnGeometricRotateRight();
+
+	afx_msg void OnLutAdd();
+	afx_msg void OnLutSub();
+	afx_msg void OnLutMultiply();
+	afx_msg void OnLutDivide();
+	afx_msg void OnLutNegative();
+	afx_msg void OnLutGamma();
+	afx_msg void OnUpdateLutAdd(CCmdUI *pCmdUI);
+	afx_msg void OnUpdateLutSub(CCmdUI *pCmdUI);
+	afx_msg void OnUpdateLutMultiply(CCmdUI *pCmdUI);
+	afx_msg void OnUpdateLutDivide(CCmdUI *pCmdUI);
+	afx_msg void OnUpdateLutNegative(CCmdUI *pCmdUI);
+	afx_msg void OnUpdateLutGamma(CCmdUI *pCmdUI);
 };
 
